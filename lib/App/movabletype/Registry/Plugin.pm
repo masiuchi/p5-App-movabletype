e1ebf7958c3e9d5a701fbf67de8e0e3cebc8ed25
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2017-11-23 16:45:31 +0900

    Implement "plug-in enable" and "plug-in disable" commands

--- lib/App/movabletype/Registry/Plugin.pm
+++ lib/App/movabletype/Registry/Plugin.pm
@@ -10,6 +10,14 @@ sub registry {
             code => \&_list,
             help => 'Get plugin list',
         },
+        enable => {
+            code => \&_enable,
+            help => 'Enable specified plugin',
+        },
+        disable => {
+            code => \&_disable,
+            help => 'Disable specified plugin',
+        },
     };
 }
 
@@ -28,12 +36,51 @@ sub _list {
 
     for my $p (@plugins) {
         next if $p->{object} && !$p->{object}->isa('MT::Plugin');
+        my $key    = $p->{key};
         my $name   = $p->{object} ? $p->{object}->label : $p->{key};
-        my $status = $p->{object} ? 'enabled'           : 'disabled';
-        $table->add( $name, $status );
+        my $status = $p->{object} ? 'enabled' : 'disabled';
+        $table->add( $key, $name, $status );
     }
 
     print $table;
+}
+
+sub _enable {
+    my ( $app, $args ) = @_;
+    my ($plugin_key) = @{ $args->{arguments} };
+    unless ($plugin_key) {
+        print "plugin key is required.\n";
+        return;
+    }
+    my $plugin = $MT::Plugins{$plugin_key};
+    unless ($plugin) {
+        print qq{"$plugin_key" does not exist.\n};
+        return;
+    }
+    my $cfg = $app->config;
+    $cfg->PluginSwitch( "$plugin_key=1", 1 );
+    $cfg->save_config;
+    $app->reboot;
+    print qq{Enabled "$plugin_key".\n};
+}
+
+sub _disable {
+    my ( $app, $args ) = @_;
+    my ($plugin_key) = @{ $args->{arguments} };
+    unless ($plugin_key) {
+        print "plugin key is required.\n";
+        return;
+    }
+    my $plugin = $MT::Plugins{$plugin_key};
+    unless ($plugin) {
+        print qq{"$plugin_key" does not exist.\n};
+        return;
+    }
+    my $cfg = $app->config;
+    $cfg->PluginSwitch( "$plugin_key=0", 1 );
+    $cfg->save_config;
+    $app->reboot;
+    print qq{Disabled "$plugin_key".\n};
 }
 
 1;
